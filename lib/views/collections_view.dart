e24359ebd69af4f9fa4d07ada051d88efa4d89ef
mart-anthony-stark <martsalazar0@gmail.com>
2023-07-07 22:07:31 +0800

    Add: onTap

--- lib/views/collections_view.dart
+++ lib/views/collections_view.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CollectionsCard extends StatelessWidget {
-  const CollectionsCard({super.key});
+  const CollectionsCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,52 +19,57 @@ class CollectionsCard extends StatelessWidget {
               elevation: 10,
               color: AppColor.SECONDARY,
               margin: const EdgeInsets.all(10),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            collectionController.collectionList[index].title,
-                            style: const TextStyle(
-                                color: Colors.white, fontSize: 18),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            collectionController
-                                .collectionList[index].description,
-                            style: const TextStyle(
-                                color: Color.fromARGB(255, 201, 201, 201),
-                                fontSize: 15),
-                          ),
-                        ],
+              child: InkWell(
+                onTap: () {
+                  debugPrint(collectionController.collectionList[index].title);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              collectionController.collectionList[index].title,
+                              style: const TextStyle(
+                                  color: Colors.white, fontSize: 18),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              collectionController
+                                  .collectionList[index].description,
+                              style: const TextStyle(
+                                  color: Color.fromARGB(255, 201, 201, 201),
+                                  fontSize: 15),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            iconSize: 20,
-                            icon: const Icon(
-                              Icons.edit,
-                              color: Colors.green,
-                            )),
-                        IconButton(
-                            onPressed: () {},
-                            iconSize: 20,
-                            icon: const Icon(
-                              Icons.delete,
-                              color: Colors.red,
-                            )),
-                      ],
-                    )
-                  ],
+                      Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {},
+                              iconSize: 20,
+                              icon: const Icon(
+                                Icons.edit,
+                                color: Colors.green,
+                              )),
+                          IconButton(
+                              onPressed: () {},
+                              iconSize: 20,
+                              icon: const Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             );
